d9ff45568bedd7f232de8584e70012838b107aac
mehdibouzit <50355811+mehdibouzit@users.noreply.github.com>
2023-10-20 07:58:22 +0000

    from 986209 to 787004 gas with suppressing unused variables / functions + uint256 to bool conversion of wasLastOdd

--- src/Gas.sol
+++ src/Gas.sol
@@ -5,38 +5,21 @@ pragma solidity 0.8.0;
 contract GasContract {
 
     address public contractOwner;
-    bool public isReady = false;
     uint256 public totalSupply; // cannot be updated
     uint256 public paymentCounter;
     mapping(address => uint256) public balances;
     mapping(address => Payment[]) public payments;
     mapping(address => uint256) public whitelist;
     address[5] public administrators;
-    enum PaymentType {
-        Unknown,
-        BasicPayment
-    }
-    PaymentType constant defaultPayment = PaymentType.Unknown;
-
-    History[] public paymentHistory; // when a payment was updated
 
     struct Payment {
-        PaymentType paymentType;
-        bool adminUpdated;
         address admin; // administrators address
-        uint256 paymentID;
-        string recipientName; // max 8 characters
         address recipient;
         uint256 amount;
     }
 
-    struct History {
-        uint256 lastUpdate;
-        address updatedBy;
-        uint256 blockNumber;
-    }
-    uint256 wasLastOdd = 1;
-    mapping(address => uint256) public isOddWhitelistUser;
+    bool wasLastOdd = true;
+    mapping(address => bool) public isOddWhitelistUser;
     
     struct ImportantStruct {
         uint256 amount;
@@ -85,13 +68,6 @@ contract GasContract {
         }
     }
 
-    function gePaymentHistory()
-        public
-        payable
-        returns (History[] memory paymentHistory_)
-    {
-        return paymentHistory;
-    }
 
     function checkForAdmin(address _user) public view returns (bool admin_) {
         bool admin = false;
@@ -137,12 +113,12 @@ contract GasContract {
             whitelist[_userAddrs] -= _tier;
             whitelist[_userAddrs] = 2;
         }
-        uint256 wasLastAddedOdd = wasLastOdd;
-        if (wasLastAddedOdd == 1) {
-            wasLastOdd = 0;
+        bool wasLastAddedOdd = wasLastOdd;
+        if (wasLastAddedOdd) {
+            wasLastOdd = false;
             isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
-        } else if (wasLastAddedOdd == 0) {
-            wasLastOdd = 1;
+        } else if (wasLastAddedOdd == false) {
+            wasLastOdd = true;
             isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
         } else {
             revert("");
